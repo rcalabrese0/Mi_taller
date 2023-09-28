1d94007337136a010f7db43e36216337238124be
rcalabrese0 <rodrigocalabrese0@gmail.com>
2023-09-28 20:58:53 -0300

    reemplazo codigo hardcodeado y realacion archivo bin a main

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
 from model.vehiculo import vehiculo
 from model.usuario import persona
 from model.instancia_cero import instancia_auto
+from bin import datos
 
 #usuario1=persona.alta_usuario()
 #auto1=vehiculo.alta_auto()
 mante1=instancia_auto()
 mante1.alta_mantenimineto()
-
-
-
-diccio_intancia_0= {
-    "c_kilometros":0,  
-    "c_aceite":11000,
-    "c_freno":30000,
-    "c_bateria":0,
-    "c_cubiertas":50000,
-    "c_correa":60000
-    }
 
 while True:
     kilometros_contador=(input("ingrese los kilometros a recorrer: "))
@@ -38,25 +28,25 @@ while True:
     FALTA LA BATERIA 
     """)
     
-    if mante1.getAceite()>=(diccio_intancia_0["c_aceite"]):
+    if mante1.getAceite()>=(datos["c_aceite"]):
         print(f"realice cambio de servicio correspondiete a : ACEITE")
         reset_aceite=input(print("A REALIZADO CAMBIO ACEITE ?(s/n)"))
         if reset_aceite=="s" or reset_aceite=="S":
             mante1.setAceite(0)
     
-    if mante1.getFreno()>=diccio_intancia_0["c_freno"]:
+    if mante1.getFreno()>=datos["c_freno"]:
         print(f"realice cambio de servicio correspondiete a : FRENO")
         reset_freno=input(print("A RENOBADO LOS FRENOS ?(s/n)"))
         if reset_freno=="s" or reset_freno=="S":
             mante1.setFreno(0)
     
-    if mante1.getCorrea()>=diccio_intancia_0["c_correa"]:
+    if mante1.getCorrea()>=datos["c_correa"]:
         print(f"realice cambio de servicio correspondiete a : CORREA")
         reset_corre=input(print("A REALIZADO CAMBIO CORREA ?(s/n)"))
         if reset_aceite=="s" or reset_aceite=="S":
             mante1.setCorrea(0)
     
-    if mante1.getCubiertas()>=diccio_intancia_0["c_cubiertas"]:
+    if mante1.getCubiertas()>=datos["c_cubiertas"]:
         print(f"realice cambio de servicio correspondiete a : CUBIERTAS")
         reset_cubiertas=input(print("A REALIZADO CAMBIO CUBIERTAS ?(S/N)"))
         if reset_cubiertas=="s" or reset_cubiertas=="S":
