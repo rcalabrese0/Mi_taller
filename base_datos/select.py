218cd1b0c65d57957de3c00a3322e145dedf405f
NICOES1320 <nicolasestebanmorales441@hotmail.com>
2023-09-28 20:23:40 -0300

    flasck , archivo bin, html y css integrados, base de datos integrada simple

--- base_datos/select.py
+++ base_datos/select.py
@@ -30,24 +30,3 @@ for fila in resultados:
 # Cerrar el cursor y la conexión
 cursor.close()
 conexion.close()
- 
-
-""" # Crear un cursor para interactuar con la base de datos
-cursor = conexion.cursor()
-
-# Sentencia SQL para insertar un nuevo registro en una tabla
-insert_query = "INSERT INTO mantenimiento_vehiculo () VALUES (%s, %s, %s,%s, %s, %s, %s)"
-
-# Datos que deseas insertar
-datos_a_insertar = (7,1,2,3,4,5,6)
-
-# Ejecutar la sentencia SQL con los datos proporcionados
-cursor.execute(insert_query, datos_a_insertar)
-
-# Confirmar la inserción
-conexion.commit()
-
-# Cerrar el cursor y la conexión
-cursor.close()
-conexion.close()
- """
---
+++ bin.py
@@ -0,0 +1,39 @@
+datos= {
+    "c_kilometros":0,  
+    "c_aceite":11000,
+    "c_freno":30000,
+    "c_bateria":0,
+    "c_cubiertas":50000,
+    "c_correa":60000
+    }
+
+
+import pickle
+
+
+
+def imprime_datos_bin():
+    # Abre el archivo en modo de lectura binaria ('rb')
+    with open('datos.bin', 'rb') as archivo_bin:
+        # Carga el diccionario desde el archivo binario
+        datos_recuperados = pickle.load(archivo_bin)
+
+    # Ahora puedes trabajar con el diccionario recuperado
+    print(datos_recuperados)
+
+# Diccionario con datos de ejemplo
+
+def carga_datos_enbin():
+    # Abre el archivo en modo de escritura binaria ('wb')
+    with open('datos.bin', 'wb') as archivo_bin:
+        # Serializa y guarda el diccionario en el archivo binario
+        pickle.dump(datos, archivo_bin)
+
+
+
+with open('datos.bin', 'rb') as archivo_bin:
+        # Carga el diccionario desde el archivo binario
+        datos_recuperados = pickle.load(archivo_bin)
+
+    # Ahora puedes trabajar con el diccionario recuperado
+        print(datos_recuperados)
