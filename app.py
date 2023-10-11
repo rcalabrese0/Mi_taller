18b33770da00881c4d6297bdccfd10b73aaf89d2
rcalabrese0 <rodrigocalabrese0@gmail.com>
2023-10-11 19:44:09 -0300

    refactor de la aplicacion

--- app.py
+++ app.py
@@ -1,16 +1,52 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
+import pymysql
 
 app = Flask(__name__)
 
+# Configura la conexión a la base de datos MySQL
+db = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="1234567",
+    database="mytaller_db",
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+
 @app.route('/')
 def formulario():
-    return render_template('index.html')
+    return render_template('addauto.html')
+
+def insertar_auto(patente, marca, modelo, color):
+    try:
+        cursor = db.cursor()
+        sql = "INSERT INTO vehiculo (patente, marca, modelo, color) VALUES (%s, %s, %s, %s)"
+        cursor.execute(sql, (patente, marca, modelo, color))
+        db.commit()
+        cursor.close()
+        return True
+    except Exception as e:
+        db.rollback()
+        return str(e)
+
 
 @app.route('/procesar', methods=['POST'])
 def procesar():
-    nombre = request.form.get('nombre')
-    return f'Hola, {nombre}'
+    patente = request.form['patente']
+    marca = request.form['marca']
+    modelo = request.form['modelo']
+    color = request.form['color']
+
+    resultado = insertar_auto(patente, marca, modelo, color)
+
+    if resultado == True:
+        mensaje = "Registro insertado con éxito"
+    else:
+        mensaje = f"Error al insertar registro: {resultado}"
+
+    return render_template('addauto.html', f_mensaje=mensaje)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
 
